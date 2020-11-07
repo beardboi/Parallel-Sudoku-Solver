11b7b30f589847a63656f6949da68473e19ac6f4
beardboi <mr.k3rn3lp4n1c@gmail.com>
2020-11-07 16:46:01 -0300

    refactor(SequentialSudoku): improve class logic.

--- src/main/java/cl/ucn/disc/hpc/SequentialSudoku.java
+++ src/main/java/cl/ucn/disc/hpc/SequentialSudoku.java
@@ -1,42 +1,43 @@
 package cl.ucn.disc.hpc;
 
-import java.util.ArrayList;
-
 public class SequentialSudoku {
 
     // The board
-    private int[][] board;
-    // The board size
-    private int boardSize;
-    //
+    private final Board board;
 
-    public SequentialSudoku(int[][] board) {
+    /**
+     * The constructor
+     *
+     * @param board The sudoku board
+     */
+    public SequentialSudoku(Board board) {
         this.board = board;
-        this.boardSize = board.length;
     }
 
     /**
      * This function solve the sudoku board using backtracking
-     * @return true if
+     *
+     * @return True if the board was solved and false if it not
      */
-    public boolean SequentialSolve() {
+    public boolean solve() {
         // Through the rows
-        for (int i = 0; i < boardSize; i++) {
+        for (int i = 0; i < board.getSize(); i++) {
             // Through the columns
-            for (int j = 0; j < boardSize; j++) {
+            for (int j = 0; j < board.getSize(); j++) {
                 // If the cells contains a 0, it means that is an empty cell
-                if (board[i][j] == 0) {
-                    // Check from 1 ... 9 to see if the insertion is valid
-                    for (int num = 1; num <= boardSize; num++) {
-                        if (isValid(board, i, j, num)) {
-                            board[i][j] = num;
+                if (board.isEmptyCell(i, j)) {
+                    // Check from 1 to 9 to see if the insertion is valid
+                    for (int num = 1; num <= board.getSize(); num++) {
+                        if (board.isValid(i, j, num)) {
+                            // Insert the value
+                            board.insert(i, j, num);
                             // Try to find out if it leads to any solution (using recursion)
-                            if (SequentialSolve()) {
+                            if (solve()) {
                                 // Return a solution
                                 return true;
                             }
                             // Undo the insertion
-                            board[i][j] = 0;
+                            board.delete(i, j);
                         }
 
                     }
@@ -47,138 +48,9 @@ public class SequentialSudoku {
             }
         }
         // Print the solution founded
-        printSudoku(board);
+
         return true;
     }
 
-
-    /**
-     * This function return the
-     *
-     * @param board     The board
-     * @param row       The row
-     * @param column    The column
-     * @param cellValue The value to insert
-     * @return TRUE for a valid insertion, FALSE in the other case.
-     */
-    public static boolean isValid(int[][] board, int row, int column, int cellValue) {
-        // If any of the functions to check returns a true, it means that the insertion goes against the constraints
-        return !(checkColumn(board, column, cellValue) || checkRow(board, row, cellValue) || checkSubgrid(board, row, column, cellValue));
-
-    }
-
-    /**
-     * This function checks the column from a board to make sure that the value to put is a valid one.
-     *
-     * @param board     The board of sudoku
-     * @param column    The column
-     * @param cellValue The value to insert
-     * @return
-     */
-    public static boolean checkColumn(int[][] board, int column, int cellValue) {
-
-        for (int[] ints : board) {
-            if (ints[column] == cellValue) {
-                return true;
-            }
-        }
-        return false;
-
-    }
-
-    /**
-     * This function checks the row from a board to make sure that the value to put is a valid one.
-     *
-     * @param board     The board
-     * @param row       The row
-     * @param cellValue The value to insert
-     * @return
-     */
-    public static boolean checkRow(int[][] board, int row, int cellValue) {
-
-        for (int i = 0; i < board.length; i++) {
-            if (board[row][i] == cellValue) {
-                return true;
-            }
-        }
-        return false;
-
-    }
-
-    /**
-     * This function checks the subgrid from a board to make sure that the value to put is a valid one.
-     *
-     * @param board     The board
-     * @param column    The column
-     * @param row       The row
-     * @param cellValue The number that will be insert in the cell
-     * @return
-     */
-    public static boolean checkSubgrid(int[][] board, int row, int column, int cellValue) {
-
-        int subGridRow = row - row % 3;
-        int subGridColumn = column - column % 3;
-
-        for (int i = subGridRow; i < subGridRow + 3; i++) {
-            for (int j = subGridColumn; j < subGridColumn + 3; j++) {
-
-                if (board[i][j] == cellValue) {
-                    return true;
-                }
-
-            }
-        }
-        return false;
-
-    }
-
-    /**
-     * This function return the numbers of empty cells from a board
-     *
-     * @param board The board
-     * @return The number of empty cells
-     */
-    public static int getNumberEmptyCells(int[][] board) {
-        // The accumulator
-        int acc = 0;
-
-        for (int[] ints : board) {
-            for (int j = 0; j < board.length; j++) {
-                //  Check if the cell is equal to 0
-                if (ints[j] == 0) acc++;
-            }
-        }
-        return acc;
-    }
-
-    /**
-     * This function show the Sudoku printed
-     *
-     * @param board The board
-     */
-    public static void printSudoku(int[][] board) {
-
-        for (int[] bytes : board) {
-
-            for (int column = 0; column < board.length; column++) {
-                System.out.print(bytes[column] + " ");
-            }
-            System.out.print("\n");
-
-        }
-
-    }
-
-    public boolean isSolved(int[][] board) {
-
-        for(int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if(board[i][j] == 0) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 
 }
