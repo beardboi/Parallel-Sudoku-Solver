be7936e1edb10afd99ce12d7b208f77877b5826e
beardboi <mr.k3rn3lp4n1c@gmail.com>
2020-11-08 05:08:39 -0300

    fix(ParallelSudoku): add fix to board size

--- src/main/java/cl/ucn/disc/hpc/ParallelSudoku.java
+++ src/main/java/cl/ucn/disc/hpc/ParallelSudoku.java
@@ -1,10 +1,16 @@
 package cl.ucn.disc.hpc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ParallelSudoku implements Runnable {
 
-
-    // The board (final?)
-    public final Board board;
+    // The sudoku board
+    private final Board board;
+    // The board size
+    private final int size;
+    // The logger
+    private final static Logger logger = LoggerFactory.getLogger(ParallelSudoku.class);
 
     /**
      * The constructor
@@ -13,6 +19,7 @@ public class ParallelSudoku implements Runnable {
      */
     public ParallelSudoku(Board board) {
         this.board = board;
+        this.size = board.getSize();
     }
 
     public boolean solve(Board board, int row, int col) {
@@ -21,19 +28,18 @@ public class ParallelSudoku implements Runnable {
         if (Board.isCompleted(board)) {
             return true;
         }
-
         // If the col reached to the end
-        if (col == 9) {
+        if (col == size) {
             row++;
             col = 0;
         }
-
         // Check if the cell is empty
-        if (!board.isEmptyCell(row, col))
-            return solve(board, row, col + 1);
+        if (!board.isEmptyCell(row, col)) {
 
+            return solve(board, row, col + 1);
+        }
         // Check from 1 to 9 if the value
-        for (int num = 1; num <= 9; num++) {
+        for (int num = 1; num <= size; num++) {
             // Check validity
             if (board.isValid(row, col, num)) {
 
@@ -48,7 +54,7 @@ public class ParallelSudoku implements Runnable {
             // Try with another number, so make the cell empty
             board.undoValue(row, col);
         }
-
+        // Solution not found
         return false;
     }
 
@@ -59,9 +65,9 @@ public class ParallelSudoku implements Runnable {
     public void run() {
         // Try to solve the board and tell if you find a solution
         if (solve(board, 0, 0)) {
-            System.out.println("Solution was found");
+            logger.info("Solution was found");
         } else {
-            System.out.println("Solution was not found");
+            logger.info("Solution was not found");
         }
     }
 
