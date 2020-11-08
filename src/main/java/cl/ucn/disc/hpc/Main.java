64743669fb9cbe9e7c64b12704bf2a3a4f6a61d8
beardboi <mr.k3rn3lp4n1c@gmail.com>
2020-11-08 05:10:14 -0300

    feat(InputFile): add method to solve using ExecutorService and solve the sudoku using N threads

--- src/main/java/cl/ucn/disc/hpc/Main.java
+++ src/main/java/cl/ucn/disc/hpc/Main.java
@@ -1,26 +1,72 @@
 package cl.ucn.disc.hpc;
 
 import java.io.File;
+import java.util.concurrent.*;
 
+import org.apache.commons.lang3.time.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The entry point of the app
+ */
 public class Main {
 
-    /*
-     * The entry point of the app.
-     */
-    public static void main(String[] args) {
+    // The maximum number of threads that the program can use
+    private static final int maxThreads = Runtime.getRuntime().availableProcessors();
+    // The minimum number of threads that the program can use
+    private static final int minThreads = 1;
+    // The logger
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+
+    public static void main(String[] args) throws InterruptedException {
         // The file .txt with the board
         File file = new File("board_test.txt");
         // Instance an InputFile to convert the txt into an array.
         InputFile boardFile = new InputFile(file);
         // Create an array that contains the sudoku board
-        int[][] board = boardFile.readBoard();
-        // Print the board
-        InputFile.printBoard(board);
-        // Solve the sudoku board sequentially using backtracking
-        SequentialSudoku sequentialSol = new SequentialSudoku(board);
-        sequentialSol.SequentialSolve();
+        int[][] initialBoard = boardFile.readBoard();
+        // Instance of a new board
+        Board board = new Board(initialBoard);
+        // Print the initial board
+        System.out.println("Initial board: ");
+        board.printBoard();
 
+        // Calculate sequentially using backtracking
+        // TODO: Add the sequential method using backtracking
 
+        // Calculate from the min to the max number of threads
+        for (int i = minThreads; i <= maxThreads; i++) {
+            solveParallel(i, board);
+        }
+
+    }
+
+    /**
+     * This function execute
+     *
+     * @param threads The number of threads to calculate the task
+     * @param board   The sudoku board
+     * @throws InterruptedException If a thread is waiting, sleeping or occupied
+     */
+    public static void solveParallel(final int threads, Board board) throws InterruptedException {
+        // The executor that manages the threads
+        final ExecutorService pool = Executors.newFixedThreadPool(threads);
+        // Give to the executor the task to submit
+        pool.submit(new ParallelSudoku(board));
+        // The executor will not receive anymore task
+        pool.shutdown();
+        // Take the execution time
+        StopWatch stopWatch = StopWatch.createStarted();
+
+        // Execute...
+        if (pool.awaitTermination(2, TimeUnit.MINUTES)) {
+            board.printBoard();
+            logger.info("The board was solved in {} ns using {} threads.", stopWatch.getNanoTime(), threads);
+        } else {
+            logger.info("The board could not be solved.");
+        }
 
     }
 
